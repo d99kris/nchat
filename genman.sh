d980f99b8d9bf1b34a537d507c8bbbb480e70800
Kristofer Berggren <kristofer.berggren@gmail.com>
2019-02-19 22:58:51 +0800

    use two cores

--- genman.sh
+++ genman.sh
@@ -2,7 +2,7 @@
 
 # genman.sh builds application and (re-)generates its man-page
 
-mkdir -p build && cd build && cmake .. && make -s && cd .. && \
+mkdir -p build && cd build && cmake .. && make -s -j2 && cd .. && \
 help2man -n "ncurses chat" -N -o src/nchat.1 ./build/bin/nchat
 exit ${?}
 
