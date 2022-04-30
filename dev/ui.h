97dd30243d8a58c0855d65670db2248b1ea65b6d
Kristofer Berggren <kristofer.berggren@gmail.com>
2022-04-30 13:29:41 +0800

    add support for building nchat without telegram protocol

---
+++ dev/ui.h
@@ -0,0 +1,40 @@
+// devui.h
+//
+// Copyright (c) 2019-2022 Kristofer Berggren
+// All rights reserved.
+//
+// nchat is distributed under the MIT license, see LICENSE for details.
+
+#pragma once
+
+#include <map>
+#include <memory>
+#include <mutex>
+#include <set>
+#include <string>
+#include <unordered_map>
+
+#include "protocol.h"
+
+class Protocol;
+class ServiceMessage;
+
+class Ui
+{
+public:
+  Ui();
+  virtual ~Ui();
+
+  void Run();
+  void AddProtocol(std::shared_ptr<Protocol> p_Protocol);
+  std::unordered_map<std::string, std::shared_ptr<Protocol>>& GetProtocols();
+  void MessageHandler(std::shared_ptr<ServiceMessage> p_ServiceMessage);
+
+private:
+  std::mutex m_StdoutMutex;
+  std::unordered_map<std::string, std::shared_ptr<Protocol>> m_Protocols;
+  std::map<std::string, std::set<std::string>> m_Chats;
+  std::map<std::string, ChatInfo> m_ChatInfos;
+  std::string m_CurrentProfileId;
+  std::string m_CurrentChatId;
+};
