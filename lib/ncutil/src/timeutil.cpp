c3128cdc8cc6dbdda2b810fff1156623a26394d4
Kristofer Berggren <kristofer.berggren@gmail.com>
2022-12-03 12:22:05 +0800

    fix short timestamp after month and year end

--- lib/ncutil/src/timeutil.cpp
+++ lib/ncutil/src/timeutil.cpp
@@ -29,32 +29,22 @@ std::string TimeUtil::GetTimeString(int64_t p_TimeSent, bool p_Short)
   struct tm tmNow;
   localtime_r(&timeNow, &tmNow);
   char tmpstr[32] = { 0 };
+  static int64_t useWeekdayMaxAge = (6 * 24 * 3600);
 
   if (p_Short)
   {
-    if (tmSent.tm_year == tmNow.tm_year)
+    if ((tmSent.tm_year == tmNow.tm_year) && (tmSent.tm_mon == tmNow.tm_mon) && (tmSent.tm_mday == tmNow.tm_mday))
     {
-      if (tmSent.tm_mon == tmNow.tm_mon)
-      {
-        if (tmSent.tm_mday == tmNow.tm_mday)
-        {
-          strftime(tmpstr, sizeof(tmpstr), "%H:%M", &tmSent);
-        }
-        else if ((tmNow.tm_mday - tmSent.tm_mday) <= 7)
-        {
-          strftime(tmpstr, sizeof(tmpstr), "%a %H:%M", &tmSent);
-        }
-        else
-        {
-          int dlen = snprintf(tmpstr, sizeof(tmpstr), "%d ", tmSent.tm_mday);
-          strftime(tmpstr + dlen, sizeof(tmpstr) - dlen, "%b %H:%M", &tmSent);
-        }
-      }
-      else
-      {
-        int dlen = snprintf(tmpstr, sizeof(tmpstr), "%d ", tmSent.tm_mday);
-        strftime(tmpstr + dlen, sizeof(tmpstr) - dlen, "%b %H:%M", &tmSent);
-      }
+      strftime(tmpstr, sizeof(tmpstr), "%H:%M", &tmSent);
+    }
+    else if ((timeNow - timeSent) <= useWeekdayMaxAge)
+    {
+      strftime(tmpstr, sizeof(tmpstr), "%a %H:%M", &tmSent);
+    }
+    else if (tmSent.tm_year == tmNow.tm_year)
+    {
+      int dlen = snprintf(tmpstr, sizeof(tmpstr), "%d ", tmSent.tm_mday);
+      strftime(tmpstr + dlen, sizeof(tmpstr) - dlen, "%b %H:%M", &tmSent);
     }
     else
     {
