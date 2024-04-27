3a4cdc72074a47e6c028cd86e289be5ff646ed90
Kristofer Berggren <kristofer.berggren@gmail.com>
2024-04-27 14:15:47 +0800

    github ci fix: macos use sudo for make install

--- make.sh
+++ make.sh
@@ -279,7 +279,11 @@ if [[ "${INSTALL}" == "1" ]]; then
   if [ "${OS}" == "Linux" ]; then
     cd build && sudo make install && cd .. || exiterr "install failed (linux), exiting."
   elif [ "${OS}" == "Darwin" ]; then
-    cd build && make install && cd .. || exiterr "install failed (mac), exiting."
+    GHSUDO=""
+    if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+      GHSUDO="sudo"
+    fi
+    cd build && ${GHSUDO} make install && cd .. || exiterr "install failed (mac), exiting."
   else
     exiterr "install failed (unsupported os ${OS}), exiting."
   fi
