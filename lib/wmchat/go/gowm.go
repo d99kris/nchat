8769a0db900fbabc31bf4838e79d8b976e5c9894
Kristofer Berggren <kristofer.berggren@gmail.com>
2026-01-11 15:22:33 +0800

    update whatsmeow to 20260107 from tulir/whatsmeow@ccfa04f

--- lib/wmchat/go/gowm.go
+++ lib/wmchat/go/gowm.go
@@ -49,7 +49,7 @@ import (
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
-var whatsmeowDate int = 20251217
+var whatsmeowDate int = 20260107
 
 type JSONMessage []json.RawMessage
 type JSONMessageType string
